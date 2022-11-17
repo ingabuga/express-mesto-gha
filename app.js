368d3cfc00199b3c32ff71ecf0bbb10aa6667457
Sanchez <ingabuga@yandex.ru>
2022-11-17 23:05:23 +0600

    create routes cards

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/users');
+// const cards = require('./routes/cards');
 
 const app = express();
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello world!!');
+// });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133',
+    _id: '637539a162602532de4985a8', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
+
   next();
 });
 
 // Подключаем Mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  // useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-  // useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/users', users);
+
+// app.use('/cards', cards);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(400).send(err));
+};
+
+const getUser = (req, res) => {
+  User.findOne({ _id: req.params.userId })
+    .orFail(() => new Error('NotFound'))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      } else {
+        res.status(500).send({ message: 'Ошибка сервера' });
+      }
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error(err.name, '=', err.message);
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        console.error('err =', err.message);
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
+    });
+};
+
+module.exports = { getUsers, getUser, createUser };
