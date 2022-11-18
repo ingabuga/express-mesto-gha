e2626e6ca8c25c7b2e4200b12a38b34ad38a206d
Sanchez <ingabuga@yandex.ru>
2022-11-18 19:30:28 +0600

    fix

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./routes/users');
-// const cards = require('./routes/cards');
+const bodyParser = require('body-parser');
+
+const NOT_FOUND_CODE = 404;
+const NOT_FOUND_MESSAGE = 'Данные не найдены';
+
+// const { NOT_FOUND_CODE, NOT_FOUND_MESSAGE } = require('./utils/constants');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
-
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '637539a162602532de4985a8', // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use('/', (req, res, next) => {
+  req.user = {
+    _id: '637539a162602532de4985a8',
+  };
   next();
 });
 
 // Подключаем Mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use('/users', users);
+app.use('/users', require('./routes/users'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.use((req, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: NOT_FOUND_MESSAGE });
 });
+
+app.listen(PORT);
