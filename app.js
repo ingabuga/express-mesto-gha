a87e38308205357d49480ee7f2c875447bee0fa2
Sanchez <ingabuga@yandex.ru>
2022-11-18 22:45:03 +0600

    add router

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   req.user = {
     _id: '66377b473a27d23cb67389774',
   };
