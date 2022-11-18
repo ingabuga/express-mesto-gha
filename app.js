e201783ce677fdd3a07d8933d57461f6c12a7392
Sanchez <ingabuga@yandex.ru>
2022-11-19 01:07:13 +0600

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { PAGE_NOT_FOUND_ERROR, PAGE_NOT_FOUND_MESSAGE } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,13 +13,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/', (req, res, next) => {
   req.user = {
-    _id: '66377b473a27d23cb67389774',
+    _id: '6377d4ed2bec3f253f899062',
   };
   next();
 });
 
 app.use('/users', require('./routers/users'));
-app.use('/users', require('./routers/cards'));
+app.use('/cards', require('./routers/cards'));
+
+app.use((req, res) => {
+  res.status(PAGE_NOT_FOUND_ERROR).send({ message: PAGE_NOT_FOUND_MESSAGE });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
