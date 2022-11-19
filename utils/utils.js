4dfde98fd878810e4f69da721733940a4b83f866
Sanchez <ingabuga@yandex.ru>
2022-11-20 00:13:28 +0600

    delete console utils

--- utils/utils.js
+++ utils/utils.js
@@ -5,22 +5,14 @@ const {
   ITERNAL_SERVER_MESSAGE,
 } = require('./constants');
 
-function handleLog(err) {
-  console.log(err.message);
-}
-
 const handleError = (err, res) => {
   if (err.name === 'NotFoundError' || err.name === 'BadRequestError') {
-    handleLog(err);
     res.status(err.code).send({ message: err.message });
   } else if (err.name === 'CastError') {
-    handleLog(err);
     res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
   } else if (err.name === 'ValidationError') {
-    handleLog(err);
     res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
   } else {
-    handleLog(err);
     res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
   }
 };
