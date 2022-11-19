aaf36e83a49069b6541b1ea72ea806c669d48cd0
Sanchez <ingabuga@yandex.ru>
2022-11-20 01:19:03 +0600

    add promise

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,13 @@
 const Card = require('../models/card');
 const { handleError } = require('../utils/utils');
 const { NotFoundError } = require('../error/NotFoundError');
+const {
+  BAD_REQUEST_ERROR,
+  ITERNAL_SERVER_ERROR,
+  BAD_REQUEST_MESSAGE,
+  OK_CODE,
+  ITERNAL_SERVER_MESSAGE,
+} = require('../utils/constants');
 
 const getCards = (req, res) => {
   Card.find({})
@@ -9,18 +16,33 @@ const getCards = (req, res) => {
     .catch((err) => handleError(err, res));
 };
 
+// const createCard = (req, res) => {
+//   const { name, link } = req.body;
+//   Card.create({ name, link, owner: req.user._id }, (err, newCard) => {
+//     if (err) {
+//       handleError(err, res);
+//       return;
+//     }
+//     Card.findById(newCard._id)
+//       .populate(['owner', 'likes'])
+//       .then((card) => res.send({ data: card }))
+//       .catch((evt) => handleError(evt, res)); //
+//   });
+// };
+
 const createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user._id }, (err, newCard) => {
-    if (err) {
-      handleError(err, res);
-      return;
-    }
-    Card.findById(newCard._id)
-      .populate(['owner', 'likes'])
-      .then((card) => res.send({ data: card }))
-      .catch((evt) => handleError(evt, res)); //
-  });
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.status(OK_CODE).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error(err.name, '=', err.message);
+        res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
+      } else {
+        console.error('err =', err.message);
+        res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
+      }
+    });
 };
 
 const removeCard = (req, res) => {
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,7 @@
 const BAD_REQUEST_ERROR = 400;
 const PAGE_NOT_FOUND_ERROR = 404;
 const ITERNAL_SERVER_ERROR = 500;
+const OK_CODE = 200;
 const BAD_REQUEST_MESSAGE = 'Данные не корректны';
 const PAGE_NOT_FOUND_MESSAGE = 'Не найдены данные';
 const ITERNAL_SERVER_MESSAGE = 'Произошла ошибка';
@@ -10,6 +11,7 @@ module.exports = {
   PAGE_NOT_FOUND_ERROR,
   ITERNAL_SERVER_ERROR,
   BAD_REQUEST_MESSAGE,
+  OK_CODE,
   PAGE_NOT_FOUND_MESSAGE,
   ITERNAL_SERVER_MESSAGE,
 };
