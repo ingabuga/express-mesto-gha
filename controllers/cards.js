e2a74bf78563612daba79782bba4ec9cfa586b3a
Sanchez <ingabuga@yandex.ru>
2022-11-20 03:19:50 +0600

    removecard errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -41,8 +41,6 @@ const removeCard = (req, res) => {
         res.status(err.code).send({ message: err.message });
       } else if (err.name === 'CastError') {
         res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
-      } else if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
       } else {
         res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
       }
