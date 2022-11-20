d6fd8ea7751cfa05b4f92b22d0d2d5f0ab30e28b
Sanchez <ingabuga@yandex.ru>
2022-11-20 16:21:09 +0600

    fix getCards

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,9 +11,14 @@ const {
 
 const getCards = (req, res) => {
   Card.find({})
-    .populate(['owner', 'likes'])
     .then((cards) => res.status(OK_CODE).send({ data: cards }))
-    .catch((err) => res.status(BAD_REQUEST_ERROR).send(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
+      } else {
+        res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
+      }
+    });
 };
 
 const createCard = (req, res) => {
