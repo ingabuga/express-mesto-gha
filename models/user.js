6f20eac7861479d8955a35ef6dc8980327b76875
Sanchez <ingabuga@yandex.ru>
2022-11-26 16:16:13 +0600

    add email

--- models/user.js
+++ models/user.js
@@ -17,6 +17,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
