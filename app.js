67136f3e6d3dc2ae9fa8999d453d08c9ab2cf7c2
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:22:47 +0600

    fix bodyParser

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { handleError } = require('./utils/utils');
@@ -8,8 +8,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
