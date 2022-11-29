8d524ea7adf433519357add5ed9309b3f9be407f
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:13:52 +0600

    fix app

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { handleError } = require('./utils/utils');
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
-
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect(DB_URL);
-
-// app.use('/', (req, res, next) => {
-//   req.user = {
-//     _id: '6377d4ed2bec3f253f899062',
-//   };
-//   next();
-// });
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/', require('./routers/index'));
 
