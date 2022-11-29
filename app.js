dd029bb40792403a226f7c65c65553e062bc1d50
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:51:03 +0600

    fix regEx

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const router = require('express').Router();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
 const { handleError } = require('./utils/utils');
-const NotFoundError = require('./errors/NotFoundError');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { REG_EXP } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,50 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string()
-        .email()
-        .required(),
-      password: Joi.string()
-        .required(),
-    }),
-  }),
-  login,
-);
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string()
-        .min(2)
-        .max(30),
-      about: Joi.string()
-        .min(2)
-        .max(30),
-      avatar: Joi.string()
-        .pattern(REG_EXP),
-      email: Joi.string()
-        .email()
-        .required(),
-      password: Joi.string()
-        .required(),
-    }),
-  }),
-  createUser,
-);
-
-router.use('/users', auth, require('./routers/users'));
-router.use('/cards', auth, require('./routers/cards'));
-
-router.use(errors());
-router.use(() => {
-  throw new NotFoundError();
-});
-
-// app.use('/', require('./routers/index'));
+app.use('/', require('./routers/index'));
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
--- utils/constants.js
+++ utils/constants.js
@@ -1,39 +1,18 @@
 const BAD_REQUEST_CODE = 400;
-
 const WRONG_LOGIN_CODE = 401;
-
 const FORBIDDEN_CODE = 403;
-
 const NOT_FOUND_CODE = 404;
-
 const EMAIL_CONFLICT_CODE = 409;
-
 const DEFAULT_ERROR_CODE = 500;
-
 const CREATED_CODE = 201;
-
 const BAD_REQUEST_MESSAGE = 'Некорректные данные';
-
 const NOT_FOUND_MESSAGE = 'Данные не найдены';
-
 const WRONG_LOGIN_MESSAGE = 'Неправильные почта или пароль';
-
 const NEED_AUTH_MESSAGE = 'Необходима авторизация';
-
 const EMAIL_CONFLICT_MESSAGE = 'Такой email уже зарегистрирован';
-
 const FORBIDDEN_MESSAGE = 'Ошибка доступа';
-
 const DEFAULT_ERROR_MESSAGE = 'Произошла ошибка';
-
-const DEFAULT_USER_NAME = 'Жак-Ив Кусто';
-
-const DEFAULT_USER_ABOUT = 'Исследователь';
-
-const DEFAULT_AVATAR_LINK = 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png';
-
-const LINK_REG_EXP = /^https?:\/\/(www\.)?[\w\d@:%~#=.+-/]+$/;
-
+const REG_EXP = /^https?:\/\/(www\.)?[\w\d@:%~#=.+-/]+$/;
 const SECRET_KEY = 'some-secret-key';
 
 module.exports = {
@@ -51,9 +30,6 @@ module.exports = {
   EMAIL_CONFLICT_MESSAGE,
   FORBIDDEN_MESSAGE,
   DEFAULT_ERROR_MESSAGE,
-  DEFAULT_USER_NAME,
-  DEFAULT_USER_ABOUT,
-  DEFAULT_AVATAR_LINK,
-  LINK_REG_EXP,
+  REG_EXP,
   SECRET_KEY,
 };
