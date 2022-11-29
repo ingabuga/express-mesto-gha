43ce004530bbc7658f463a966d6ba00c7de6ce7c
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:28:12 +0600

    find some secret key

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { handleError } = require('./utils/utils');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+
 const app = express();
 
 app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use('/', require('./routers/index'));
 
