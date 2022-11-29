8cd9b0d2d235ec6086657f3d5905bb7b6b816323
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:37:36 +0600

    mongo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { handleError } = require('./utils/utils');
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(DB_URL);
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/', require('./routers/index'));
 
