d8bcd3009aa500bf2cff80fe5757747fbcfdc75c
Sanchez <ingabuga@yandex.ru>
2022-11-30 01:00:40 +0600

    fix model user

--- models/user.js
+++ models/user.js
@@ -1,32 +1,25 @@
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
-const DataAccessError = require('../errors/DataAccessError');
-const NotFoundError = require('../errors/NotFoundError');
-
-const {
-  DEFAULT_USER_NAME,
-  DEFAULT_USER_ABOUT,
-  DEFAULT_AVATAR_LINK,
-} = require('../utils/constants');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      default: DEFAULT_USER_NAME,
+      default: 'Жак-Ив Кусто',
       minlength: 2,
       maxlength: 30,
     },
     about: {
       type: String,
-      default: DEFAULT_USER_ABOUT,
+      default: 'Исследователь океана',
       minlength: 2,
       maxlength: 30,
     },
     avatar: {
       type: String,
-      default: DEFAULT_AVATAR_LINK,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validator: (link) => validator.isEmail(link),
+      message: 'Неверный формат ссылки',
     },
     email: {
       type: String,
@@ -34,45 +27,13 @@ const userSchema = new mongoose.Schema(
       unique: true,
       validate: {
         validator: (email) => validator.isEmail(email),
+        message: 'Неверный формат емейла',
       },
     },
     password: {
       type: String,
       required: true,
       select: false,
-    },
-  },
-  {
-    statics: {
-      findUserByCredentials(email, password) {
-        return this.findOne({ email }).select('+password')
-          .orFail(() => {
-            throw new DataAccessError();
-          })
-          .then((user) => bcrypt.compare(password, user.password)
-            .then((matched) => {
-              if (!matched) {
-                throw new DataAccessError();
-              }
-              return user;
-            }));
-      },
-      findUserById(id, res, next) {
-        return this.findById(id)
-          .orFail(() => {
-            throw new NotFoundError();
-          })
-          .then((user) => res.send({ data: user }))
-          .catch(next);
-      },
-      updateUserData(id, res, next, params) {
-        return this.findByIdAndUpdate(id, params, { new: true, runValidators: true })
-          .orFail(() => {
-            throw new NotFoundError();
-          })
-          .then((user) => res.send({ data: user }))
-          .catch(next);
-      },
     },
   },
 );
