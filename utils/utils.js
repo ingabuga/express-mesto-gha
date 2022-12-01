4190838c090eae7a7f25f3f1e3f59c9da32965fc
Sanchez <ingabuga@yandex.ru>
2022-12-01 19:58:16 +0600

    fix handleError

--- utils/utils.js
+++ utils/utils.js
@@ -1,30 +1,31 @@
-const CustomError = require('../errors/CustomError');
+// const CustomError = require('../errors/CustomError');
 const {
-  BAD_REQUEST_ERROR,
-  BAD_REQUEST_MESSAGE,
   ITERNAL_SERVER_ERROR,
   ITERNAL_SERVER_MESSAGE,
-  EMAIL_ERROR,
-  EMAIL_MESSAGE,
 } = require('./constants');
 
-function handleLog(err) {
-  console.log(err.message);
-}
+// function handleLog(err) {
+//   console.log(err.message);
+// }
 
-const handleError = (err, res) => {
-  if (err instanceof CustomError) {
-    err.sendError(res);
-  } else if (err.name === 'ValidationError') {
-    res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
-  } else if (err.name === 'CastError') {
-    res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
-  } else if (err.code === 11000) {
-    res.status(EMAIL_ERROR).send({ message: EMAIL_MESSAGE });
-  } else {
-    handleLog(err);
-    res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
-  }
-};
+// const handleError = (err, res) => {
+//   if (err instanceof CustomError) {
+//     err.sendError(res);
+//   } else if (err.name === 'ValidationError') {
+//     res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
+//   } else if (err.name === 'CastError') {
+//     res.status(BAD_REQUEST_ERROR).send({ message: BAD_REQUEST_MESSAGE });
+//   } else if (err.code === 11000) {
+//     res.status(EMAIL_ERROR).send({ message: EMAIL_MESSAGE });
+//   } else {
+//     handleLog(err);
+//     res.status(ITERNAL_SERVER_ERROR).send({ message: ITERNAL_SERVER_MESSAGE });
+//   }
+// };
+
+const handleError = ((err, req, res, next) => {
+  res.status(ITERNAL_SERVER_ERROR).send({ ITERNAL_SERVER_MESSAGE });
+  next();
+});
 
 module.exports = { handleError };
