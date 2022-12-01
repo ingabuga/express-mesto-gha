9767b400664160ac6077f16d101a164baca0c979
Sanchez <ingabuga@yandex.ru>
2022-12-02 03:06:25 +0600

    createUser

--- errors/ConflictError.js
+++ errors/ConflictError.js
@@ -1,14 +1,23 @@
-const {
-  EMAIL_ERROR, EMAIL_MESSAGE,
-} = require('../utils/constants');
-const CustomError = require('./CustomError');
+// const {
+//   EMAIL_ERROR, EMAIL_MESSAGE,
+// } = require('../utils/constants');
+// const CustomError = require('./CustomError');
 
-class ConflictError extends CustomError {
-  constructor() {
-    super();
-    this.name = 'ConflictError';
-    this.message = EMAIL_MESSAGE;
-    this.code = EMAIL_ERROR;
+// class ConflictError extends CustomError {
+//   constructor() {
+//     super();
+//     this.name = 'ConflictError';
+//     this.message = EMAIL_MESSAGE;
+//     this.code = EMAIL_ERROR;
+//   }
+// }
+
+// module.exports = ConflictError;
+
+class ConflictError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
   }
 }
 
