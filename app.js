9577d5d41a55660bbe95c7f766ce5d62f10de1d4
Sanchez <ingabuga@yandex.ru>
2022-12-01 20:09:05 +0600

    refactoring app

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { handleError } = require('./utils/utils');
+const {
+  ITERNAL_SERVER_ERROR,
+  ITERNAL_SERVER_MESSAGE,
+} = require('./utils/constants');
+// const { handleError } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 
@@ -19,8 +23,13 @@ app.use('/', require('./routers/index'));
 
 app.use(errors());
 // eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  handleError(err, res);
+// app.use((err, req, res, next) => {
+//   handleError(err, res);
+// });
+
+app.use((req, res, next) => {
+  res.status(ITERNAL_SERVER_ERROR).send({ ITERNAL_SERVER_MESSAGE });
+  next();
 });
 
 app.listen(PORT, () => {
