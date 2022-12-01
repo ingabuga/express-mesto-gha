d9ff59d4e0e9ebd0d578cf24b9729a49ffb9d92a
Sanchez <ingabuga@yandex.ru>
2022-12-02 00:30:44 +0600

    badRequest

--- errors/BadRequestError.js
+++ errors/BadRequestError.js
@@ -1,14 +1,21 @@
-const {
-  BAD_REQUEST_ERROR, BAD_REQUEST_MESSAGE,
-} = require('../utils/constants');
-const CustomError = require('./CustomError');
+// const {
+//   BAD_REQUEST_ERROR, BAD_REQUEST_MESSAGE,
+// } = require('../utils/constants');
+// const CustomError = require('./CustomError');
 
-class BadRequestError extends CustomError {
-  constructor() {
-    super();
-    this.name = 'BadRequestError';
-    this.message = BAD_REQUEST_MESSAGE;
-    this.code = BAD_REQUEST_ERROR;
+// class BadRequestError extends CustomError {
+//   constructor() {
+//     super();
+//     this.name = 'BadRequestError';
+//     this.message = BAD_REQUEST_MESSAGE;
+//     this.code = BAD_REQUEST_ERROR;
+//   }
+// }
+
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
   }
 }
 
