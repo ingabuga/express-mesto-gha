d238901aa1faf2dbdf0f7e78808cb3f717300a56
Sanchez <ingabuga@yandex.ru>
2022-12-01 23:35:32 +0600

    add handler to app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { handleError } = require('./utils/utils');
+// const { handleError } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 
@@ -19,8 +19,14 @@ app.use('/', require('./routers/index'));
 
 app.use(errors());
 // eslint-disable-next-line no-unused-vars
+// app.use((err, req, res, next) => {
+//   handleError(err, res);
+// });
+
 app.use((err, req, res, next) => {
-  handleError(err, res);
+  const { statusCode = 500, message = 'Ошибка сервера' } = err;
+  res.status(statusCode).send({ message });
+  next();
 });
 
 app.listen(PORT, () => {
