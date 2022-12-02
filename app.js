62781a212b7f1f00366b3b77887036deee437314
Sanchez <ingabuga@yandex.ru>
2022-12-02 17:51:18 +0600

    modify app

--- app.js
+++ app.js
@@ -18,16 +18,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use('/', require('./routers/index'));
 
 app.use(errors());
-// eslint-disable-next-line no-unused-vars
-// app.use((err, req, res, next) => {
-//   handleError(err, res);
-// });
+
 app.use(handleError);
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message = 'Ошибка сервера' } = err;
-//   res.status(statusCode).send({ message });
-//   next();
-// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
