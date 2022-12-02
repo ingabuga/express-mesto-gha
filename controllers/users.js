2f097e671256f0c8f1f2d0955249f4ad99378839
Sanchez <ingabuga@yandex.ru>
2022-12-02 16:33:42 +0600

    fix controller users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-// const { CREATED_ERROR, EMAIL_MESSAGE, BAD_REQUEST_MESSAGE } = require('../utils/constants');
 const BadRequestError = require('../errors/BadRequestError');
-// const AuthError = require('../errors/AuthError');
 const NotFoundError = require('../errors/NotFoundError');
 const EmailError = require('../errors/EmailError');
 
@@ -34,43 +32,6 @@ const getUser = (req, res, next) => {
     });
 };
 
-// const createUser = (req, res, next) => {
-//   const {
-//     name,
-//     about,
-//     avatar,
-//     password,
-//     email,
-//   } = req.body;
-
-//   bcrypt.hash(password, 10)
-//     .then((hash) => User.create({
-//       name,
-//       about,
-//       avatar,
-//       email,
-//       password: hash,
-//     }))
-//     .then((user) => res.status(CREATED_ERROR).send({
-//       data: {
-//         name: user.name,
-//         about: user.about,
-//         avatar: user.avatar,
-//         email: user.email,
-//         _id: user._id,
-//       },
-//     }))
-//     .catch((err) => {
-//       if (err.code === 11000) {
-//         return next(new ConflictError(EMAIL_MESSAGE));
-//       }
-//       if (err.name === 'ValidationError') {
-//         return next(new BadRequestError(BAD_REQUEST_MESSAGE));
-//       }
-//       return next(err);
-//     });
-// };
-
 const createUser = (req, res, next) => {
   const {
     name,
@@ -92,16 +53,6 @@ const createUser = (req, res, next) => {
       delete newUser.password;
       res.send(newUser);
     })
-    // .catch((err) => {
-    //   if (err.code === 11000) {
-    //     next(new EmailError('Пользователь с таким email уже существует'));
-    //   } else if (err.name === 'ValidationError') {
-    //     next(new BadRequestError('Переданы некорректные данные'));
-    //   } else {
-    //     next(err);
-    //   }
-    // });
-
     .catch((err) => {
       if (err.code === 11000) {
         return next(new EmailError('Пользователь с таким email уже существует'));
