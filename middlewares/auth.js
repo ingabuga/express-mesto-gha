1aa2c1c1126954a19deeaba0a3cfdca3d9c803bd
Sanchez <ingabuga@yandex.ru>
2022-12-02 17:38:00 +0600

    modify auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
-const DataAccessError = require('../errors/AuthError');
+const AuthError = require('../errors/AuthError');
 const { AUTH_MESSAGE } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (!token) {
-    next(new DataAccessError(AUTH_MESSAGE));
+    next(new AuthError(AUTH_MESSAGE));
     return;
   }
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new DataAccessError(AUTH_MESSAGE));
+    next(new AuthError(AUTH_MESSAGE));
   }
   req.user = payload;
   next();
