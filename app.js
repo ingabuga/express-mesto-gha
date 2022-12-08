958ac80e4c883c53c80ece2c3cf4a2b1e8aff0b5
Sanchez <ingabuga@yandex.ru>
2022-12-09 01:35:38 +0600

    add logger

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleError } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
@@ -15,7 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(requestLogger);
+
 app.use('/', require('./routers/index'));
+
+app.use(errorLogger);
 
 app.use(errors());
 
